9af91b6707f3de18f2d926ae77733ea745374f06
aishvarya779 <aishvaryatiwari123@gmail.com>
2019-11-25 15:12:28 +0530

    footer added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Footer = () => {
-  return <footer>This is Footer</footer>;
+  return (
+    <footer className="py-5 bg-dark">
+      <div className="container">
+        <p className="m-0 text-center text-white">
+          Copyright © Aishvarya Tiwari 2019
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
